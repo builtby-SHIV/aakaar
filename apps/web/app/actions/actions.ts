ce17f6626c3b79b5a50ebd3790794c7076e860fb
Shiv <shivshukla00514@gmail.com>
2026-09-17 18:58:15 +0530

    add: new action to produce jobs in redis streams

--- apps/web/app/actions/actions.ts
+++ apps/web/app/actions/actions.ts
@@ -1,5 +1,6 @@
 'use server';
 
+import { getRedisConnection } from "../../lib/redis";
 import { signIn, signOut } from "../auth";
 
 export async function googleLogIn (formData: FormData) {
@@ -12,4 +13,9 @@ export async function googleLogOut (formData: FormData) {
     const action = formData.get('action');
     if (typeof action === 'string')
         await signOut({redirectTo: '/'});
+}
+
+export async function addToRedisStream (videoId: number) {
+    const client = await getRedisConnection();
+    client.xAdd('videos', '*', { videoId: videoId.toString() });
 }
